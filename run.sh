b6eefd684a4776b82ac50888fe650135ddf2cb0b
Lan Gong <lan.gong@sambanovasystems.com>
2025-04-24 07:39:55 -0700

    add cache dir to run script

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+ACTUAL_CACHE=/import/ml-sc-scratch6/lang/cache/
+
+export HF_DATASET_CACHE=${ACTUAL_CACHE}
+export HF_MODULES_CACHE=${ACTUAL_CACHE}
+export HF_HOME=${ACTUAL_CACHE}
+
 llamafactory-cli train examples/train_full/llama_finetuning_on_llava_med_gpu.yaml
